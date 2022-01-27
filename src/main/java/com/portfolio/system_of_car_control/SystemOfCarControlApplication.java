f8abad85dd5dbee4917c4134cf2129ab247026db
Magnetto2 <imalysev869@gmail.com>
2022-01-27 18:17:01 +0200

    Initial commit

---
+++ src/main/java/com/portfolio/system_of_car_control/SystemOfCarControlApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.system_of_car_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemOfCarControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemOfCarControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/system_of_car_control/SystemOfCarControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.system_of_car_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemOfCarControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
